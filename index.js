ee127549e70edfc187d76c5ca99fd0c02f303ebc
gabriele.lana <gabriele.lana@cleancode.it>
2012-11-02 12:00:09 +0100

    execute index.js only when is called directly

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var NJSW2012 = module.exports = {
   what: 'the best workshop in the world'
 }
 
-console.log("Welcome to %s", NJSW2012.what)
+if (module.parent === null) {
+  console.log("Welcome to %s", NJSW2012.what)
+}
